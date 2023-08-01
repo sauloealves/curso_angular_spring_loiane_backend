005b80551c5786fac852c6a9ebd548b53a7d02e9
saulo <sauloealves@gmail.com>
2023-07-31 22:10:13 -0300

    delete, edit

--- src/main/java/com/saulo/curdspring/controller/CourseController.java
+++ src/main/java/com/saulo/curdspring/controller/CourseController.java
@@ -2,12 +2,26 @@ package com.saulo.curdspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.saulo.curdspring.model.Course;
 import com.saulo.curdspring.repository.CourseRepository;
+
+import io.micrometer.core.ipc.http.HttpSender.Response;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("/api/courses")
@@ -20,8 +34,53 @@ public class CourseController {
     }
 
     @GetMapping
-    public List<Course> list(){
+    public List<Course> list() {
         return courseRepository.findAll();
     }
+
+    // @RequestMapping(method=RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
+    // PODE SER DA FORMA ABAIXO
+    /*
+    // @RequestMapping(method=RequestMethod.POST)
+    @PostMapping
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(courseRepository.save(course));
+    } */    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getById(@PathVariable long id){
+        return courseRepository.findById(id)
+        .map(recordFound -> ResponseEntity.ok().body(recordFound))
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable long id, @RequestBody Course course){
+        return courseRepository.findById(id)
+        .map(recordFound -> {
+            recordFound.setName((course.getName()));
+            recordFound.setCategory(course.getCategory());
+            Course updated = courseRepository.save(recordFound);
+            return ResponseEntity.ok().body(updated);
+        })
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id){
+        return courseRepository.findById(id)
+        .map(recordFound -> {
+            courseRepository.delete(recordFound);
+            return ResponseEntity.noContent().<Void>build();
+        })
+        .orElse(ResponseEntity.notFound().build());
+    }
+
 
 }
